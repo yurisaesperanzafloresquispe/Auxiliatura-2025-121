165df436f34981ffb5c1a554228439a54be8a6a0
yurisaesperanzafloresquispe <yfloresq@fcpn.edu.bo>
2025-12-06 16:29:39 -0400

    Add Libro class with properties and methods

--- PRACTICA-3/ejer6/libro.java
+++ PRACTICA-3/ejer6/libro.java
@@ -1,1 +1,22 @@
+package ejer6;
+public class Libro {
+    private int codLibro;
+    private String titulo;
+    private double precio;
+
+    public Libro(int codLibro, String titulo, double precio) {
+        this.codLibro = codLibro;
+        this.titulo = titulo;
+        this.precio = precio;
+    }
+
+    public int getCodLibro() { return codLibro; }
+    public String getTitulo() { return titulo; }
+    public double getPrecio() { return precio; }
+
+    @Override
+    public String toString() {
+        return codLibro + " - " + titulo + " - Bs." + precio;
+    }
+}
 
