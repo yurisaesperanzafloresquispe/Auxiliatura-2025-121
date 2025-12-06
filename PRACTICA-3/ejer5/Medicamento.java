63c6efab2c76f01e9b5c4ab0544aea22ff8bce7b
yurisaesperanzafloresquispe <yfloresq@fcpn.edu.bo>
2025-12-06 16:22:30 -0400

    5

--- PRACTICA-3/ejer5/Medicamento.java
+++ PRACTICA-3/ejer5/Medicamento.java
@@ -1,1 +1,33 @@
 
+package ejer5;
+public class Medicamento {
+    private String nombre;
+    private int codMedicamento;
+    private String tipo;
+    private double precio;
+
+    public Medicamento() {}
+
+    public void leer() {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.print("Nombre: ");
+        nombre = sc.nextLine();
+        System.out.print("Código: ");
+        codMedicamento = sc.nextInt(); sc.nextLine();
+        System.out.print("Tipo: ");
+        tipo = sc.nextLine();
+        System.out.print("Precio: ");
+        precio = sc.nextDouble(); sc.nextLine();
+    }
+
+    public void mostrar() {
+        System.out.println("Nombre: " + nombre 
+            + " | Código: " + codMedicamento 
+            + " | Tipo: " + tipo 
+            + " | Precio: " + precio);
+    }
+
+    public String getTipo() { return tipo; }
+    public double getPrecio() { return precio; }
+    public String getNombre() { return nombre; }
+}
