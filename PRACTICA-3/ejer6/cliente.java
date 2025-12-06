5ce05bc9526568d3b60df8e7b5500e3ae53a75c0
yurisaesperanzafloresquispe <yfloresq@fcpn.edu.bo>
2025-12-06 16:29:14 -0400

    Implement Cliente class with constructor and methods

--- PRACTICA-3/ejer6/cliente.java
+++ PRACTICA-3/ejer6/cliente.java
@@ -1,1 +1,23 @@
+package ejer6;
+
+public class Cliente {
+    private int codCliente;
+    private String ci;
+    private String nombre;
+    private String apellido;
+
+    public Cliente(int codCliente, String ci, String nombre, String apellido) {
+        this.codCliente = codCliente;
+        this.ci = ci;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getCodCliente() { return codCliente; }
+
+    @Override
+    public String toString() {
+        return codCliente + " - " + nombre + " " + apellido;
+    }
+}
 
