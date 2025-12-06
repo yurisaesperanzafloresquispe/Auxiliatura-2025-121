743a149fbcbde29e68bb4b43900c80046eccac9a
yurisaesperanzafloresquispe <yfloresq@fcpn.edu.bo>
2025-12-06 16:28:37 -0400

    Implement ArchPrestamo class for loan management

--- PRACTICA-3/ejer6/ArchPrestamo.java
+++ PRACTICA-3/ejer6/ArchPrestamo.java
@@ -1,1 +1,40 @@
+package ejer6;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class ArchPrestamo {
+    private ArrayList<Prestamo> prestamos = new ArrayList<>();
+    private String nombreArch = "prestamos.json";
+
+    public void adicionar(Prestamo p) {
+        prestamos.add(p);
+        guardarArchivo();
+    }
+
+    public ArrayList<Prestamo> getPrestamos() { return prestamos; }
+
+    public void guardarArchivo() {
+        try (FileWriter writer = new FileWriter(nombreArch)) {
+            Gson gson = new Gson();
+            writer.write(gson.toJson(prestamos));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cargarArchivo() {
+        try {
+            Gson gson = new Gson();
+            Reader reader = Files.newBufferedReader(Paths.get(nombreArch));
+            prestamos = gson.fromJson(reader, new TypeToken<ArrayList<Prestamo>>(){}.getType());
+            reader.close();
+        } catch (Exception e) {
+            prestamos = new ArrayList<>();
+        }
+    }
+}
 
