4683b3abd4018c8f1d220263ed86f52131028226
yurisaesperanzafloresquispe <yfloresq@fcpn.edu.bo>
2025-12-06 16:37:49 -0400

    Update Principal.java

--- PRACTICA-3/ejer7/Principal.java
+++ PRACTICA-3/ejer7/Principal.java
@@ -1,1 +1,37 @@
+package ejer7;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        ArchNiño archivo = new ArchNiño(); 
+
+        archivo.leerDatos(); 
+
+        archivo.listar();
+
+        System.out.println("\nb) Peso adecuado = " + archivo.contarPesoAdecuado());
+
+        System.out.println("\nc) Niños NO adecuados:");
+        archivo.mostrarNoAdecuados();
+
+        System.out.println("\nd) Promedio de edad = " + archivo.promedioEdad());
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nIngrese CI a buscar: ");
+        int x = sc.nextInt();
+
+        Niño buscado = archivo.buscarPorCi(x);
+
+        if (buscado != null) {
+            System.out.println("\nNiño encontrado:");
+            buscado.mostrar();
+        } else {
+            System.out.println("No existe ese CI.");
+        }
+
+        archivo.mostrarTallaMayor();
+    }
+}
 
